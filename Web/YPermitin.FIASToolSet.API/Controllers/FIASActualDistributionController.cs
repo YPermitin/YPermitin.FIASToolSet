c15be08287d5fba8aaa0e50c734d82ec52de65d6
Permitin Yury <ypermitin@yandex.ru>
2022-09-14 16:41:20 +0500

    Улучшения описания API для Swagger

--- Web/YPermitin.FIASToolSet.API/Controllers/FIASActualDistributionController.cs
+++ Web/YPermitin.FIASToolSet.API/Controllers/FIASActualDistributionController.cs
@@ -9,7 +9,7 @@ namespace YPermitin.FIASToolSet.API.Controllers
     /// Работа с актуальной информацией о ФИАС.
     /// Получается напрямую от ФНС и может быть использована для проверки актуальности ФИАС.
     /// </summary>
-    [ApiExplorerSettings(GroupName = "ФИАС. Актуальная информация")]
+    [ApiExplorerSettings(GroupName = "ФИАС. Браузер дистрибутивов")]
     [ApiController]
     [Route("FIAS/actualDistribution")]
     public class FIASActualDistributionController : ControllerBase
