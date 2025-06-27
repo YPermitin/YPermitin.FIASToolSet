be0cc39adf5449ed6355d73c5e8faea1f00c9b58
ypermitin <ypermitin@yandex.ru>
2025-06-27 22:07:24 +0500

    Небольшие улучшения
    - Улучшена конвертация строк в веб-адреса.
    - Расширен набор тестов для методов-расширения.

--- Libs/YPermitin.FIASToolSet.DistributionBrowser/Extensions/StringExtensions.cs
+++ Libs/YPermitin.FIASToolSet.DistributionBrowser/Extensions/StringExtensions.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("YPermitin.FIASToolSet.API.Tests")]
 namespace YPermitin.FIASToolSet.DistributionBrowser.Extensions
-{
-    internal static class StringExtensions
+{    
+    public static class StringExtensions
     {
-        internal static Uri ToAbsoluteUri(this string sourceValue) 
+        public static Uri ToAbsoluteUri(this string sourceValue) 
         {
+            if(string.IsNullOrEmpty(sourceValue))
+            {
+                return null;
+            }
+
             if(Uri.TryCreate(sourceValue, UriKind.Absolute, out Uri parsedUri))
             {
                 return parsedUri;
---
+++ Tests/YPermitin.FIASToolSet.DistributionBrowser.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using YPermitin.FIASToolSet.DistributionBrowser.Extensions;
+
+namespace YPermitin.FIASToolSet.DistributionBrowser.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void ToAbsoluteUri_Basic_Test()
+        {
+            string uriAsString = "https://www.google.com";
+            Uri correctUri = new Uri(uriAsString);
+
+            Uri parsedUri = uriAsString.ToAbsoluteUri();
+
+            Assert.Equal(correctUri, parsedUri);
+        }
+
+        [Fact]
+        public void ToAbsoluteUri_Null_Test()
+        {
+            string uriAsString = null;
+            Uri correctUri = null;
+
+            Uri parsedUri = uriAsString.ToAbsoluteUri();
+
+            Assert.Equal(correctUri, parsedUri);
+        }
+
+        [Fact]
+        public void ToAbsoluteUri_Empty_Test()
+        {
+            string uriAsString = string.Empty;
+            Uri correctUri = null;
+
+            Uri parsedUri = uriAsString.ToAbsoluteUri();
+
+            Assert.Equal(correctUri, parsedUri);
+        }
+    }
+}
